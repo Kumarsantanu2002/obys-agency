98b7657540a20284cb7b071e7bab999f72dc746a
Santanu <san481896@gmail.com>
2024-09-19 23:34:19 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-
+function loadingAnimation()
+{
 // var h5timer= document.querySelector("#line1-part1 h5")
 // var grow=0;
-
 // setInterval(function(){
 //   if(grow<100)
 //   {
@@ -11,8 +11,7 @@
 //   else{
 //     grow=100;
 //     h5timer.innerHTML=grow;
-//   }
-  
+//   }  
 // },50 )
 
 var tl=gsap.timeline()
@@ -50,7 +49,7 @@ tl.to(".line h2",{
 
 tl.to("#loader",{
   opacity:0,
-  duration:0.4,
+  duration:0.2,
    delay:2
 })
 
@@ -58,10 +57,36 @@ tl.from("#page1",{
   // delay:0.2,
   y:1600,
   opacity:0,
-  duration:0.5,
+  duration:0.6,
   ease:Power4
 })
 tl.to("#loader",{
   display:"none",
 
 })
+tl.from("#nav",{
+  opacity:0
+})
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+  y:120,
+  stagger:0.2
+})
+}
+
+function cursorAnimation(){
+  document.addEventListener("mousemove",function(dets){
+    gsap.to("#crsr",{
+     left:dets.x,
+     top:dets.y
+    })
+ })
+ 
+ Shery.makeMagnet("#nav-part2 h4" /* Element to target.*/, {
+   //Parameters are optional.
+   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+   duration: 1,
+ });
+}
+loadingAnimation()
+cursorAnimation();
+
